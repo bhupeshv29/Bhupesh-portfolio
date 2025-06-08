3fd11004a4f204d226475450dfb93e3bb1f93501
bhupesh <bhupeshverma29bv@gmail.com>
2025-06-08 19:40:27 +0530

    resume added

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ function Home() {
             </p>
             <div>
               <a
-                href="https://drive.google.com/file/d/1WkfJkp0flwovUnkKUL2jbdsvSTWKBYa7/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1lOP0YIaOi1GBSab2-0Q3vZm9VD-xqHTs/view?usp=drive_link"
                 target="_blank"
                 download="bhupesh_resume.pdf"
               >
