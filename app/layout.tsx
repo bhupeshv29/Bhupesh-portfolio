73a0d23df55a4323c0bee123a8d3ffda0bb43575
bhupesh <bhupeshverma29bv@gmail.com>
2026-01-07 17:55:07 +0530

    add: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SocialDock from "../components/contact";
-
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          <SocialDock />
+        <SocialDock />
 
-          {children}
+        {children}
+        <Analytics />
       </body>
     </html>
   );
