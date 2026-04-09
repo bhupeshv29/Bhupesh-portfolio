0bf4d3caa9ced57afea2256f2ec08d4ffb92d901
bhupesh <bhupeshverma29bv@gmail.com>
2026-04-09 08:51:11 +0530

    link fixed

--- components/OpenSourceContribution.tsx
+++ components/OpenSourceContribution.tsx
@@ -9,7 +9,7 @@ function OpenSourceContribution() {
         subtitle="os contribution"
         icon={<GoGitPullRequest className="h-6 w-6 text-zinc-300" />}
         value="Pr"
-        href=""
+        href="https://github.com/search?q=is:pr+author:bhupeshv29+-user:bhupeshv29+-repo:piyushgarg-dev/webdev-cohort-opensource+-repo:hiteshchoudhary/open-source-contribution&type=pullrequests"
         className="top-1"
       />
     </div>
